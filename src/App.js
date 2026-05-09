33276ae6620e5e29462b2c147724a256a10dbcd7
Krystian Kluba <klubakrystian@gmail.com>
2026-05-09 16:22:24 +0200

    app js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/NavBar/NavBar.js';
+import { Banner } from './components/Banner/Banner.js';
+import { Skills } from './components/Skills/Skills.js';
+import { Projects } from './components/Projects/Projects.js';
+import { Contact } from './components/Contact/Contact.js';
+import { Footer } from './components/Footer/Footer.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Footer />
     </div>
   );
 }
